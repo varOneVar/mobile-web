13b4c86376385778d8151ac4bd02402dde03fce2
song qing <vn0bn6u@email.wal-mart.com>
2020-05-07 18:24:43 +0800

    feat(登录) 账号列表 错误提示与校验

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="{offline:!isOnline}">
+    <md-notice-bar
+      v-if="!isOnline"
+      class="offline-tip"
+      icon="warn"
+      mode="closable"
+      type="warning"
+    >
+      网络连接中断！
+    </md-notice-bar>
+    <router-view />
+    <div v-if="wholePageLoading" class="mask">
+      <md-skeleton v-for="item of 6" :key="item" avatar title />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import { mapGetters, mapMutations } from 'vuex'
+import { simulateInterval } from '@/utils'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    ...mapMutations('app', ['SET_WHOLE_PAGE_LOADING', 'SET_CONTENT_LOADING'])
+  },
+  created() {
+    this.SET_CONTENT_LOADING(false)
+    this.SET_WHOLE_PAGE_LOADING(false)
+    this.$logger.INIT()
+    // 可以做一个错误日志上报的， 每两天清理一下logger
+    simulateInterval(this.$logger.clean, 2 * 24 * 60 * 60 * 1000, true)
+  },
+  computed: {
+    ...mapGetters(['isOnline', 'wholePageLoading'])
   }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
+  height: 100%;
+}
+.offline {
+  box-sizing border-box
+  padding-top: 32px;
+}
+.offline-tip {
+  position fixed
+  left 0
+  right 0
+  top 0
+}
+.mask {
+  position fixed
+  left 0
+  top 0
+  bottom 0
+  right 0
+  background #fff
+  z-index 5000
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'normalize.css/normalize.css'
+
+import './icons'
+import './plugins/mand-mobile'
+import '@/styles/index.styl'
+import dayjs from 'dayjs'
+
+// store引用要在routeq=前面，不然会出现间接的循环引用，导致异常
+import store from '@/store'
+import router from '@/router'
+import './permission'
+import { deepClone } from '@/utils'
+import logger from '@/utils/logs'
+import setting from './setting'
+import directive from './directive'
+Vue.use(directive)
 
 Vue.config.productionTip = false
 
+// Vue.config.errorHandler = function (err, vm, info) {
+//   console.log(err, vm, info)
+//   // handle error
+//   // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
+//   // 只在 2.2.0+ 可用
+// }
+
+Object.defineProperties(Vue.prototype, {
+  $cloneDeep: {
+    value: deepClone
+  },
+  $dayjs: {
+    value: dayjs
+  },
+  $logger: {
+    value: logger
+  },
+  SUCCESS_CODE: {
+    value: setting.success_code
+  }
+})
+
 new Vue({
-  render: h => h(App),
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
